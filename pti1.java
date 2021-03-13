aedf573f5514f1c496e673c3f785085f73aa7a39
adrianocarneiro90 <66155891+adrianocarneiro90@users.noreply.github.com>
2021-03-13 16:29:03 -0300

    Update pti1.java

--- pti1.java
+++ pti1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class pti1 {
+public class Main {
 	public static void main(String[] args) {
 		Scanner leitor = new Scanner(System.in);
 		System.out.print("Quantidade elementos: ");
